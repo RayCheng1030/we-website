4caf357d976e777aafe9ddaa41f3f840a49280d1
Puzz Chang <espirit.mail@gmail.com>
2022-05-25 21:10:25 +0800

    Apply the markdown feature

--- src/views/pages/ArticlePage.vue
+++ src/views/pages/ArticlePage.vue
@@ -11,7 +11,7 @@
                         <img class="lNewsDetail-lead-img wp-post-image" :src="require(`@/assets/images/articles/${ $t(`${ article }.image`) }`)" width="1000" height="650" alt="" />
                     </div>
                     <div class="lNewsDetail-wysiwyg">
-                        <p v-for="(paragraph, i) in paragraphs" :key="i">{{ paragraph }}</p>
+                        <p v-for="(paragraph, i) in paragraphs" :key="i" v-html="md.renderInline(paragraph)"></p>
                     </div>
                     <ContactLink :title="$t(`button.back`)" href="/news" :routerLink="true" />
                 </div>
@@ -24,9 +24,11 @@
 </template>
 
 <script setup lang="ts">
+import { RenderRule } from "markdown-it/lib/renderer";
 import { computed, onMounted } from "vue";
 import { getI18nList } from "@/models/util";
 import { useRoute } from "vue-router";
+import markdownIt from "markdown-it";
 
 import ContactLink from "@/views/comps/ContactLink.vue";
 
@@ -34,6 +36,26 @@ const { params } = useRoute();
 const article = computed(() => `news.pages.${ params.id }`);
 
 const paragraphs = getI18nList(`${ article.value }.content`);
+
+const underline = (md: markdownIt) => {
+    const renderEm = ((tokens, idx, options, env, self) => {
+        const token = tokens[idx];
+
+        if (token.markup === "_")
+            token.tag = "u";
+        return self.renderToken(tokens, idx, options);
+    }) as RenderRule;
+
+    md.renderer.rules.em_open = renderEm;
+    md.renderer.rules.em_close = renderEm;
+}
+
+const md = markdownIt({
+    html: true,
+    xhtmlOut: true,
+    breaks: true,
+    linkify: true,
+}).use(underline);
 
 onMounted(async () => {
     const head = document.getElementsByTagName("head")[0];
