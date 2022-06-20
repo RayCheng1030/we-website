f7cfbf9ca8e5a2575605fc2f7c74af550afb9fab
Puzz Chang <espirit.mail@gmail.com>
2022-06-20 16:07:27 +0800

    Rewrite the marquee effects for dynamic text

--- src/views/comps/BottomMarquee.vue
+++ src/views/comps/BottomMarquee.vue
@@ -1,7 +1,7 @@
 <template>
     <p :class="`cMarquee ${ props.target }-marquee`" aria-label="Marquee Text" data-ref>
-        <span class="cMarquee-line" data-ref>
-            <span class="cMarquee-text" data-ref>
+        <span class="cMarquee-line" :style="`transform: translateX(-${ marquee.offsetX }px)`" data-ref>
+            <span class="cMarquee-text" ref="marqueeText" data-ref>
                 <span v-for="(text, i) in texts" :key="i">
                     <span v-if="i !== 0">&nbsp;&nbsp;&nbsp;&nbsp;</span>
                     <span>{{ text }}</span>
@@ -12,11 +12,32 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineProps } from "vue";
+import { computed, defineProps, reactive, ref } from "vue";
 import { getI18nList } from "@/models/util";
+
+const marqueeText = ref(null as HTMLSpanElement | null);
+const marquee = reactive({
+    textWidth: 0,
+    offsetX: 0,
+    speed: 0.5,
+});
 
 const props = defineProps<{ target: string }>();
 const texts = computed(() => getI18nList(`marquee.bottom`));
+
+const getMarqueeWidth = () => {
+    if (marqueeText.value) {
+        const { width } = marqueeText.value.getBoundingClientRect();
+
+        width && (marquee.textWidth = width);
+        marquee.offsetX += marquee.speed;
+
+        if (marquee.offsetX > marquee.textWidth)
+            marquee.offsetX = 0;
+    }
+    requestAnimationFrame(getMarqueeWidth);
+};
+requestAnimationFrame(getMarqueeWidth);
 </script>
 
 <style lang="scss">
